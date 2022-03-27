727be877b6e8bd8577e66bcdaf93487958c4908b
noormohammad20 <mdsaim204@gmail.com>
2022-03-28 01:44:06 +0600

    Use reachart to draw any type of chart using React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg'
 import './App.css'
+import MyLineChart from './components/MyLineChart/MyLineChart'
 
 function App() {
   return (
     <div className="App">
-
+      <MyLineChart></MyLineChart>
     </div>
   )
 }
