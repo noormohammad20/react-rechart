a07e9bab11ea679993042d5eba4f517157a50c8b
noormohammad20 <mdsaim204@gmail.com>
2022-03-28 02:03:15 +0600

    Axios, data extraction and data processing bar chart

---
+++ src/components/SpecialChart/SpecialChart.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const SpecialChart = () => {
+    const [phones, setPhones] = useState([])
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SpecialChart
