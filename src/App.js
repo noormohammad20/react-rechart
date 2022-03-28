337aa404165be08b5eac7994f0bc0fe88016395f
noormohammad20 <mdsaim204@gmail.com>
2022-03-28 11:40:31 +0600

    react spring animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css'
 import MyLineChart from './components/MyLineChart/MyLineChart'
+import { useSpring, animated } from 'react-spring'
+import { useState } from 'react'
+import SpecialChart from './components/SpecialChart/SpecialChart'
 
 function App() {
+  const [flip, set] = useState(false)
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 200,
+    onRest: () => set(!flip),
+  })
+
   return (
+
     <div className="App">
+      < animated.div style={props} > I will fade in</ animated.div>
       <MyLineChart></MyLineChart>
+      <SpecialChart></SpecialChart>
     </div>
   )
 }
